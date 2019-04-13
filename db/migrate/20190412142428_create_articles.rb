2f002474cfe2cb447b81f6252a1bd35fb2971bec
Vladholovchak <vlad.golovchak.1996@gmail.com>
2019-04-13 23:55:42 +0300

    fix default published_at

--- db/migrate/20190412142428_create_articles.rb
+++ db/migrate/20190412142428_create_articles.rb
@@ -8,7 +8,7 @@ class CreateArticles < ActiveRecord::Migration[5.2]
       t.string :description, null: false, default: ''
       t.string :url, null: false, default: ''
       t.string :url_to_image, null: false, default: ''
-      t.datetime :published_at, null: false, default: Time.current
+      t.datetime :published_at, null: false, default: '2019-04-13 17:48:19'
       t.string :content, null: false, default: ''
       t.timestamps
     end
