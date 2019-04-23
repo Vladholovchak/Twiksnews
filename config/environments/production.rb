c0e3346e0dd15c9428613602dd77f0941813bc72
Vladholovchak <vlad.golovchak.1996@gmail.com>
2019-04-24 00:22:50 +0300

    Mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   end
 
   config.action_mailer.delivery_method = :smtp
-
+  config.action_mailer.default_url_options = { :host => 'twiksnews.herokuapp.com' }
   config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
       :port                 => 587,
