c08460c6d9ddc1dca2728fc6f1089d327de991b3
Vladholovchak <vlad.golovchak.1996@gmail.com>
2019-04-23 22:06:46 +0300

    comments

--- app/controllers/news_fetchers_controller.rb
+++ app/controllers/news_fetchers_controller.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
-class NewsFetchersController < ApplicationController
-  def fetch_news
-    authorize User
-    UpdateNewsJob.set(wait: 10.seconds).perform_later
-    redirect_to root_path
-    flash[:success] = 'Articles will be updated after 10 seconds!'
-  end
-
-  def fetch_sources
-    authorize User
-    UpdateSourcesJob.set(wait: 10.seconds).perform_later
-    redirect_to root_path
-    flash[:success] = 'Sources will be updated after 10 seconds!'
-  end
-
-  def user_params
-    params.require(@user).permit(:id)
-  end
-end
+# class NewsFetchersController < ApplicationController
+#   def fetch_news
+#     authorize User
+#     UpdateNewsJob.set(wait: 10.seconds).perform_later
+#     redirect_to root_path
+#     flash[:success] = 'Articles will be updated after 10 seconds!'
+#   end
+#
+#   def fetch_sources
+#     authorize User
+#     UpdateSourcesJob.set(wait: 10.seconds).perform_later
+#     redirect_to root_path
+#     flash[:success] = 'Sources will be updated after 10 seconds!'
+#   end
+#
+#   def user_params
+#     params.require(@user).permit(:id)
+#   end
+# end
--- app/services/fetch_articles_service.rb
+++ app/services/fetch_articles_service.rb
@@ -1,29 +1,29 @@
 # frozen_string_literal: true
-
-class FetchArticlesService
-  def call
-    @api_key = Rails.application.credentials.api_key
-    @newsapi = News.new(@api_key)
-    @parsed_info = @newsapi.get_everything(q: 'news')
-  end
-
-  def create_sources
-    call
-    @parsed_info.each do |info_fields|
-      ::Source.find_or_create_by(name: info_fields.name)
-    end
-  end
-
-  def create_articles
-    call
-    @sources = ::Source.all
-    @parsed_info.each do |info_fields|
-      Article.find_or_create_by(source: @sources.find_by_name(info_fields.name),
-                                title: info_fields.title,
-                                description: info_fields.description,
-                                url: info_fields.url,
-                                url_to_image: info_fields.urlToImage,
-                                published_at: info_fields.publishedAt)
-    end
-  end
-end
+#
+# class FetchArticlesService
+#   def call
+#     @api_key = Rails.application.credentials.api_key
+#     @newsapi = News.new(@api_key)
+#     @parsed_info = @newsapi.get_everything(q: 'news')
+#   end
+#
+#   def create_sources
+#     call
+#     @parsed_info.each do |info_fields|
+#       ::Source.find_or_create_by(name: info_fields.name)
+#     end
+#   end
+#
+#   def create_articles
+#     call
+#     @sources = ::Source.all
+#     @parsed_info.each do |info_fields|
+#       Article.find_or_create_by(source: @sources.find_by_name(info_fields.name),
+#                                 title: info_fields.title,
+#                                 description: info_fields.description,
+#                                 url: info_fields.url,
+#                                 url_to_image: info_fields.urlToImage,
+#                                 published_at: info_fields.publishedAt)
+#     end
+#   end
+# end
