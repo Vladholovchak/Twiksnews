0440bbfc65245261e07b7c2398c5714e5d7db104
Vladholovchak <vlad.golovchak.1996@gmail.com>
2019-04-23 23:57:50 +0300

    email settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,17 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :user_name            => Rails.application.credentials.gmail_user_name,
+      :password             => Rails.application.credentials.gmail_user_password,
+      :authentication       => "plain",
+      :enable_starttls_auto => true
+  }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
