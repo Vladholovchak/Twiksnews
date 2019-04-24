a930be8d21965ddb9cfa7d5beeacbdca9d0bd459
Vladholovchak <vlad.golovchak.1996@gmail.com>
2019-04-24 15:21:41 +0300

    change mail sidekiq

--- app/jobs/news_mailer_job.rb
+++ app/jobs/news_mailer_job.rb
@@ -3,7 +3,7 @@
 class NewsMailerJob < ApplicationJob
   queue_as :default
   def perform
-    users = User.all.where(send_news: true)
+    users = User.all.where(send_news: true, status: activated)
     users.each do |user|
       if user.articles.present?
         NewsMailer.news_email(user).deliver
