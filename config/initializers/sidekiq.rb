6c390e7f67df76beb8765cb09275f00746bd0007
Vladholovchak <vlad.golovchak.1996@gmail.com>
2019-04-24 14:25:59 +0300

    change shedule

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
-schedule_file = 'config/schedule.yml'
-
-Sidekiq.configure_server do |config|
   schedule_file = 'config/schedule.yml'
   if File.exist?(schedule_file) && Sidekiq.server?
     Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
   end
-end
+
 
