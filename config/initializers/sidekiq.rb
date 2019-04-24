4b480f8e8a163767d26007f30bcafe073cebacd7
Vladholovchak <vlad.golovchak.1996@gmail.com>
2019-04-24 03:12:25 +0300

    Sidekiq in prod

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,13 +3,9 @@
 schedule_file = 'config/schedule.yml'
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'REDIS_URL' }
   schedule_file = 'config/schedule.yml'
   if File.exist?(schedule_file) && Sidekiq.server?
     Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
   end
 end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: 'REDIS_URL' }
-end
