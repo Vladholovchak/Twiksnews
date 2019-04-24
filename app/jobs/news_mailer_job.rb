efce91d5fdc7785185b94272b3130412ad9f9f01
Vladholovchak <vlad.golovchak.1996@gmail.com>
2019-04-24 14:04:04 +0300

    change shedule

--- app/jobs/news_mailer_job.rb
+++ app/jobs/news_mailer_job.rb
@@ -4,7 +4,7 @@ class NewsMailerJob < ApplicationJob
   queue_as :default
   def perform
     users = User.all.where(send_news: true)
-    users.each do |user| if users.
+    users.each do |user|
       if user.articles.present? && user.status.activated?
         NewsMailer.news_email(user).deliver
       end
